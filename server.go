dce253ac9117fc844099845e5ed92c3d215db24d
k4sud0n <k4sud0n@gmail.com>
2024-06-27 17:47:58 +0900

    add database

--- server.go
+++ server.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"get-on/ent"
+
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	app := fiber.New()
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
-	type User struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
+func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
 	}
+	defer client.Close()
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	app := fiber.New()
 
 	// 회원가입
 	app.Post("/api/v1/users", func(c *fiber.Ctx) error {
@@ -23,6 +38,7 @@ func main() {
 				"error": "Invalid input data",
 			})
 		} else {
+			CreateUser(client, user.Username, user.Password)
 			c.SendStatus(201)
 			return c.JSON(user)
 		}
@@ -45,5 +61,28 @@ func main() {
 		}
 	})
 
+	// 회원조회
+	app.Get("/api/v1/users", func(c *fiber.Ctx) error {
+		u, err := client.User.Query().All(context.TODO())
+		if err != nil {
+			log.Fatal(err)
+		}
+		return c.JSON(u)
+	})
+
 	app.Listen(":3000")
+}
+
+func CreateUser(client *ent.Client, username string, password string) *ent.User {
+	u, err := client.User.
+		Create().
+		SetUsername(username).
+		SetPassword(password).
+		Save(context.TODO())
+
+	if err != nil {
+		log.Fatalf("failed creating user: %v", err)
+	}
+
+	return u
 }
