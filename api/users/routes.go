1b80fac39b6981e7a186e03c62a8eabc1fba4119
k4sud0n <k4sud0n@gmail.com>
2024-06-28 13:14:13 +0900

    add view user

--- api/users/routes.go
+++ api/users/routes.go
@@ -4,5 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 func Routes(route fiber.Router) {
 	route.Post("/users", CreateUser)
-	route.Get("/users", ReadUser)
+	route.Get("/users", ReadAllUser)
+	route.Get("/users/:id", ReadUser)
 }
--- api/users/users.go
+++ api/users/users.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"get-on/database"
+	"get-on/ent"
+	userField "get-on/ent/user"
 	"get-on/model"
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"strconv"
 )
 
 func CreateUser(c *fiber.Ctx) error {
@@ -33,11 +36,34 @@ func CreateUser(c *fiber.Ctx) error {
 	}
 }
 
-func ReadUser(c *fiber.Ctx) error {
+func ReadAllUser(c *fiber.Ctx) error {
 	client := database.Client
-	u, err := client.User.Query().All(context.TODO())
+	user, err := client.User.Query().All(context.TODO())
 	if err != nil {
 		log.Fatal(err)
 	}
-	return c.JSON(u)
+	return c.JSON(user)
+}
+
+func ReadUser(c *fiber.Ctx) error {
+	userIdParam := c.Params("id")
+	userId, err := strconv.Atoi(userIdParam)
+
+	client := database.Client
+
+	user, err := client.User.Query().
+		Where(userField.IDEQ(userId)).
+		Only(context.TODO())
+
+	if err != nil {
+		if ent.IsNotFound(err) {
+			c.SendStatus(404)
+			return c.JSON(fiber.Map{
+				"error": "User not found",
+			})
+		}
+		log.Fatal(err)
+	}
+
+	return c.JSON(user)
 }
